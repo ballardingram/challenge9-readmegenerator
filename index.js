c6516fddd7c873a9045cbea45a74818eeed1459e
Ballard Ingram <ballardingram@outlook.com>
2022-04-22 10:33:05 -0500

    validation prompts added and tested to be working!

--- index.js
+++ index.js
@@ -1,19 +1,40 @@
-// These are the external packages needed.
+// EXTERNAL PACKAGES > INQUIRER AND UTIL INSTALLED, FS DOES NOT REQUIRE INSTALL
+// DOCUMENTATION > INQUIRER (https://www.npmjs.com/package/inquirer)
+// DOCUMENTATION > UTIL (https://www.npmjs.com/package/util)
+// DOCUMENTATION > FS (https://nodejs.dev/learn/the-nodejs-fs-module)
 const inquirer = require('inquirer');
 const fs = require('fs');
 const util = require('util');
 
-// List of prompts for the read me generator.
+// CONST > QUESTIONS FOR README - This was pulled from the module example. I used inquirer documentation (https://www.npmjs.com/package/inquirer) to test 'types' for the questions so they were not all just text inputs.
+// DOCUMENTATION > PROFESSIONAL README GUIDE (https://coding-boot-camp.github.io/full-stack/github/professional-readme-guide)
+// NOTE: I read over the documentation for the Professional ReadMe Guide and some of the questions I changed based on my experience reading other readmes and what I think would be helpful to see in a README. I want to work on adding MEDIA QUESTIONS to the readme, but I am not there yet.
 const readmequestions = [
     {
         type: "input",
         name: "title",
         message: "What is the project title?",
+        validate: titleInput => {
+            if (titleInput) {
+                return true;
+            } else {
+                console.log('Lets start with a Project Title! This will make your readme PROFESSIONAL.')
+                return false;
+            }
+        }
     },
     {
         type: "input",
         name: "description",
         message: "What is the project description?",
+        validate: descriptionInput => {
+            if (descriptionInput) {
+                return true;
+            } else {
+                console.log('Dont leave us guessing. Tell us what the project is about.')
+                return false;
+            }
+        }
     },
     {
         type: "checkbox",
@@ -25,16 +46,40 @@ const readmequestions = [
         type: "input",
         name:"installation",
         message: "How should users install your code?",
+        validate: installationInput => {
+            if (installationInput) {
+                return true;
+            } else {
+                console.log('This helps everyone build a Professional ReadMe, tell us how you did it.')
+                return false;
+            }
+        }
     },
     {
         type: "input",
         name: "collaboration",
         message: "What collaboration was done for this project? Who did you work with? How was the contribution delegated?",
+        validate: collaborationInput => {
+            if (collaborationInput) {
+                return true;
+            } else {
+                console.log('If you didnt work with anyone, thats cool. Tell us this was a solo project. Youre a rockstar.')
+                return false;
+            }
+        }
     },
     {
         type: "input",
         name: "challenges",
         message: "What opportunities and challenges did you have with this project?",
+        validate: challengesInput => {
+            if (challengesInput) {
+                return true;
+            } else {
+                console.log('None? Whatsoever? I think you can do better than that. What did you learn?')
+                return false;
+            }
+        }
     },
     {
         type: "confirm",
@@ -50,8 +95,16 @@ const readmequestions = [
     },
     {
         type: "input",
-        name: "github username",
+        name: "github_username",
         message: "What is your GitHub username?",
+        validate: github_usernameInput => {
+            if (github_usernameInput) {
+                return true;
+            } else {
+                console.log('Dont be shy! Where can we find your project?')
+                return false;
+            }
+        }
     },
 ];
 
@@ -66,6 +119,7 @@ function writeToFile(fileName, data) {
 }
 
 // TODO: Create a function to initialize app
+// TODO > STATUS COMPLETED
 function init() {
     inquirer
     .prompt(readmequestions)
